0f9a091d235fac45728fb93f152cddf4168e187a
kataslon <kataslon@yandex.ru>
2016-05-13 18:58:00 +0300

    add VotingsController with specs

--- app/controllers/votings_controller.rb
+++ app/controllers/votings_controller.rb
@@ -1,13 +1,43 @@
 class VotingsController < ApplicationController
 
-	before_action :set_voting, except: [:index]
+	before_action :set_voting, except: [:index, :new, :create]
 	before_action :authenticate_user!
 
 	def index
 		@votings = current_user.votings
 	end
 
+	def new
+		@voting = Voting.new
+	end
+
+	def create
+		@voting = Voting.create(voting_params)
+		if @voting.save
+			redirect_to votings_url
+		else
+			redirect_to new_voting_url
+		end
+	end
+
 	def show
+	end
+
+	def edit
+	end
+
+	def update
+		@voting.update_attributes(voting_params)
+		if @voting.save
+			redirect_to votings_url
+		else
+			redirect_to edit_voting_url(@voting)
+		end
+	end
+
+	def destroy
+		@voting.destroy
+		redirect_to votings_url
 	end
 
 	private
@@ -17,6 +47,6 @@ class VotingsController < ApplicationController
 	end
 
 	def voting_params
-		params.require(:voting).permit(:description)
+		params.require(:voting).permit(:polls_count, :description, :user_id)
 	end
 end
--- spec/controllers/votings_controller_spec.rb
+++ spec/controllers/votings_controller_spec.rb
@@ -1,5 +1,107 @@
 require 'rails_helper'
 
 RSpec.describe VotingsController, type: :controller do
+	let!(:user) { create(:user) }
+	let!(:voting) { create(:voting, user_id: user.id) }
+	let!(:invited_user) { create(:user) }
+	let!(:count) { user.votings.count }
+	7.times do
+		let!(:opinion) { create(:opinion, voting_id: voting.id, user_id: invited_user.id) }
+	end
+
+	context 'when user is logged in' do
+		before :each do
+			sign_in user
+		end
+
+		describe 'and user initiated vote' do
+
+			describe 'GET#index' do
+				it 'renders index template' do
+					get :index
+					expect(response).to render_template :index
+				end
+			end
+
+			describe 'GET#new' do
+				it 'renders new template' do
+					get :new
+					expect(response).to render_template :new
+				end
+			end
+
+			describe 'POST#create' do
+				describe 'whith inconsistent parameters' do
+					it 'render new template' do
+						post :create, voting: { polls_count: 3 }
+						expect(response).to redirect_to new_voting_path
+					end
+				end
+
+				describe 'with correct parameters' do
+					it 'creates new voting' do
+						expect{post :create, voting: { polls_count: 7 } }.to change(Voting, :count).by(1)
+					end
+
+					it 'redirect to index template' do
+						post :create, voting: { description: 'first voting' }
+						expect(response).to redirect_to votings_path
+					end
+				end
+			end
+
+			describe 'GET#show' do
+				it 'renders show template' do
+					get :show, id: voting
+					expect(response).to render_template :show
+				end
+			end
+
+			describe 'GET#edit' do
+				it 'renders edit template' do
+					get :edit, id: voting
+					expect(response).to render_template :edit
+				end
+			end
+
+			describe 'PUTCH#update' do
+				describe 'whith inconsistent parameters' do
+					it 'does not change voting' do
+						patch :update, id: voting, voting: { polls_count: nil }
+            expect(voting.reload.polls_count).to eq voting.polls_count
+						expect(response).to redirect_to edit_voting_path
+					end
+				end
+
+				describe 'whith inconsistent parameters' do
+					it 'updates voting properties' do
+						new_polls_count = 7
+						patch :update, id: voting, voting: { polls_count: new_polls_count }
+            expect(voting.reload.polls_count).to eq new_polls_count
+						expect(response).to redirect_to votings_path
+					end
+				end
+			end
+
+			describe 'DELETE#destroy' do
+				it 'renders destroy template' do
+					voting
+					expect{ patch :destroy, id: voting }.to change(Voting, :count).by(-1)
+          expect{ Voting.find(voting.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+					expect(response).to redirect_to votings_path
+				end
+			end
+
+		end
+
+		describe 'and user is invited' do
+			before { loging_whith invited_user }
+		end
+	end
+
+	context 'when user is not logged in' do
+		describe 'and user is invited' do
+		end
+	end
 
 end
